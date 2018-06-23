56a055b1707b84b2bc3da9084d36abe3390f2690
Unknown <arthur.perdereau@gmail.com>
2018-06-24 01:06:07 +0200

    lourd

--- SU - Suspension/09 - LAS automatique/Results/Traitement_donnees.m
+++ SU - Suspension/09 - LAS automatique/Results/Traitement_donnees.m
@@ -1,8 +1,9 @@
 % 
-[Valeurs Texte ValeursEtTexte] = xlsread('test_2.xls');
-Rear_Suspension_Travel = Valeurs(:,5);
-Rear_Wheel_Travel = Valeurs(:,6)-Valeurs(1,6);
-Rear_MR = Rear_Wheel_Travel./Front_Suspension_Travel;
+[Valeurs Texte ValeursEtTexte] = xlsread('Bump_2.xlsx');
+Bump = Valeurs(:,1);
+Rear_Wheel_Travel = abs(Valeurs(:,5)-Valeurs(6,5));
+Rear_Suspension_Travel = abs(Valeurs(:,7)-Valeurs(6,7));
+Rear_MR = Rear_Wheel_Travel./Rear_Suspension_Travel;
 
 for i=2:length(Rear_MR)-1
     if isnan(Rear_MR(i))
@@ -10,7 +11,17 @@ for i=2:length(Rear_MR)-1
     end
 end
         
-plot(Rear_Suspension_Travel, Rear_MR)
+plot(Bump, Rear_MR)
 title('Rear Motion Ratio (Wheel/Suspension)')
-xlabel('Suspension Travel (mm)')
+xlabel('Bump (mm)')
 ylabel('Motion ratio')
+
+Front_Camber = Valeurs(:,8);
+Rear_Camber = Valeurs(:,9);
+
+
+plot(-Bump, -Front_Camber)
+title('Camber Variation')
+xlabel('Bump (mm)')
+ylabel('Camber (deg)')
+legend('Front')
